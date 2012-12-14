8f4f487cc6585fc9fac789ee16e7956421a137ba
Tangent 128 <Tangent128@gmail.com>
2012-12-14 00:53:25 -0500

    Fold resources.h into main.c

--- src/main.c
+++ src/main.c
@@ -2,7 +2,39 @@
 #include <lua.h>
 #include <lualib.h>
 #include <lauxlib.h>
-#include "resources.h"
+
+/*******************************
+ *  Macros to declare & load
+ *  C & Lua modules, according
+ *  to naming conventions 
+ */
+
+#define EXTERN_LUA_CHUNK(name) \
+	extern char res_ ## name ## _luac[]; \
+	extern int res_ ## name ## _luac_size;
+
+#define LOAD_LUA_CHUNK(L, name) \
+	EXTERN_LUA_CHUNK(name) \
+	luaL_loadbuffer( \
+	L, \
+	res_ ## name ## _luac, \
+	res_ ## name ## _luac_size, \
+	#name );
+
+#define RUN_LUA_CHUNK(L, name) \
+	LOAD_LUA_CHUNK(L, name) \
+	lua_call(L, 0, 0);
+
+#define OPEN_LUA_C_LIB(L, name) \
+	extern int luaopen_ ## name (lua_State *L); \
+	lua_pushcfunction(L, &luaopen_ ## name); \
+	lua_call(L, 0, 1); /* returns library table */ \
+	lua_setglobal(L, #name );
+
+/*******************************
+ *  Main- load C & Lua modules,
+ *        then run starting code 
+ */
 
 int main(int argc, char** argv) {
 	
